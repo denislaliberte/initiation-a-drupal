876e4767722aea35f8966e53fe78ee2333422899
Denis Laliberté <denis@tp1.ca>
2014-12-10 17:42:19 -0500

    update script

--- build.sh
+++ build.sh
@@ -4,12 +4,5 @@
 sed '/--/d' index.md |  sed '/template:/d' |sed '/class:/d' |sed '/name:/d' | sed '/???/d' | sed '/_todo_/d' | sed -e 's/^\*//' > readme.md
 
 ## build 
-#pandoc  -c flatly.css  -o index.html index.md
-
-## build 
 pandoc -o initiation-a-drupal.pdf readme.md
 
-## push to github pages
-git checkout gh-pages
-git merge master
-git push origin gh-pages
---
+++ export.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "remove remark specific markdown (slides separator, comments, slides information and todo comments)"
+sed '/--/d' index.md |  sed '/template:/d' |sed '/class:/d' |sed '/name:/d' | sed '/???/d' | sed '/_todo_/d' | sed -e 's/^\*//' > readme.md
+
+echo "export as pdf"
+pandoc -o initiation-a-drupal.pdf readme.md
